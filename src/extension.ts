371fa28e4d9c425f0736a7aec956bbdc9c09c5aa
Maina Wycliffe <wmmaina7@gmail.com>
2022-07-14 19:15:52 +0300

    Implement Rudimentary First Version

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,57 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+export function determinePackageNameForNPM(importPath: string): string {
+  return "";
+}
+
+export function buildLink(importTextLine: vscode.TextLine) {
+  // to determine a package name is a little tricky, make cross reference with
+  // because paths are included, also exclude local files
+  const packageName = importTextLine.text.split('"')[1];
+
+  const startCharacter = importTextLine.text.indexOf(packageName);
+  const endCharacter = startCharacter + packageName.length;
+  const linkRange = new vscode.Range(
+    importTextLine.lineNumber,
+    startCharacter,
+    importTextLine.lineNumber,
+    endCharacter
+  );
+
+  const linkUri = vscode.Uri.parse(
+    `https://www.npmjs.com/package/${packageName}`
+  );
+  return new vscode.DocumentLink(linkRange, linkUri);
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "link-package-npm" is now active!');
+  let disposable = vscode.languages.registerDocumentLinkProvider(
+    ["typescript", "typescriptreact", "javascript", "javascriptreact"],
+    {
+      provideDocumentLinks(document, token) {
+        console.log(document, token);
+        vscode.window.showInformationMessage(
+          "Hello World from Link Package NPM!"
+        );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('link-package-npm.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Link Package NPM!');
-	});
+        return Array.from(Array(document.lineCount).keys())
+          .map((line) => document.lineAt(line))
+          .filter((lineContent) => {
+            if (lineContent.text.includes("import")) {
+              return true;
+            }
+            return false;
+          })
+          .map((importLineText) => {
+            return buildLink(importLineText);
+          });
+      },
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
