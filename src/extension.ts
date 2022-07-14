de477758197c975b221960c9444c307eb65c12d8
Maina Wycliffe <wmmaina7@gmail.com>
2022-07-14 21:18:27 +0300

    Use CodeLens to Show Links to Package

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
 export function determinePackageNameForNPM(importPath: string): string {
-  return "";
+  // local imports, not an npm package
+  if (
+    importPath.startsWith("/") ||
+    importPath.startsWith("./") ||
+    importPath.startsWith("../") ||
+    importPath.startsWith("~")
+  ) {
+    return "";
+  }
+
+  // npm package belongs to organization or is in tsconfig
+  if (importPath.startsWith("@")) {
+    const pathsArray = importPath.split("/");
+    return `${pathsArray[0]}/${pathsArray[1]}`;
+  }
+
+  // return only the first path without a package
+  return importPath.split("/")[0];
 }
 
 export function buildLink(importTextLine: vscode.TextLine) {
   // to determine a package name is a little tricky, make cross reference with
   // because paths are included, also exclude local files
-  const packageName = importTextLine.text.split('"')[1];
+  const importLine = importTextLine.text
+    .replace("'", '"')
+    // replaceAll is currently unavaible on the target es version for this
+    // project, so excuse this dirty workaround
+    .replace("'", '"')
+    .split('"')[1];
+
+  const packageName = determinePackageNameForNPM(importLine);
+
+  if (packageName === "") {
+    return null;
+  }
 
   const startCharacter = importTextLine.text.indexOf(packageName);
   const endCharacter = startCharacter + packageName.length;
@@ -20,22 +46,20 @@ export function buildLink(importTextLine: vscode.TextLine) {
     endCharacter
   );
 
-  const linkUri = vscode.Uri.parse(
-    `https://www.npmjs.com/package/${packageName}`
-  );
-  return new vscode.DocumentLink(linkRange, linkUri);
+  const linkUri = `https://www.npmjs.com/package/${packageName}`;
+
+  return new vscode.CodeLens(linkRange, {
+    title: `View ${packageName} package on npm.js`,
+    command: "openPackageOnNPM",
+    arguments: [linkUri],
+  });
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.languages.registerDocumentLinkProvider(
+  let regCodeLensProviderDisposable = vscode.languages.registerCodeLensProvider(
     ["typescript", "typescriptreact", "javascript", "javascriptreact"],
     {
-      provideDocumentLinks(document, token) {
-        console.log(document, token);
-        vscode.window.showInformationMessage(
-          "Hello World from Link Package NPM!"
-        );
-
+      provideCodeLenses(document, token) {
         return Array.from(Array(document.lineCount).keys())
           .map((line) => document.lineAt(line))
           .filter((lineContent) => {
@@ -46,12 +70,30 @@ export function activate(context: vscode.ExtensionContext) {
           })
           .map((importLineText) => {
             return buildLink(importLineText);
-          });
+          })
+          .filter((importPath) => {
+            if (importPath) {
+              return importPath;
+            }
+            return false;
+          }) as vscode.CodeLens[];
       },
     }
   );
 
-  context.subscriptions.push(disposable);
+  let regCommandDisposable = vscode.commands.registerCommand(
+    "openPackageOnNPM",
+    async (...args) => {
+      const link = vscode.Uri.parse(args[0]);
+      await vscode.commands.executeCommand("vscode.open", link);
+      vscode.window.showInformationMessage(
+        "Link has been opened in your browser"
+      );
+    }
+  );
+
+  context.subscriptions.push(regCodeLensProviderDisposable);
+  context.subscriptions.push(regCommandDisposable);
 }
 
 // this method is called when your extension is deactivated
