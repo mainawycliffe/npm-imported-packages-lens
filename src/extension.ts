ec1448a5f4ad98dce0745807432c28155d9c9776
Maina Wycliffe <wmmaina7@gmail.com>
2022-07-17 21:12:01 +0300

    Check for null before running replace

--- src/extension.ts
+++ src/extension.ts
@@ -52,7 +52,7 @@ export function constructPackageNameFromAstNode(
   const line = document.positionAt(node.getStart());
   const text = document.lineAt(line.line);
   const packageName = determinePackageNameForNPM(
-    importPath[0].replace(/'/g, "").replace(/"/g, "").trim()
+    importPath[0]?.replace(/'/g, "")?.replace(/"/g, "").trim()
   );
   return {
     importLineText: text,
