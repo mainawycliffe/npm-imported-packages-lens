c0884d5d7a85e23da0b0f03bd517ea6c14062fa7
Richard Sugg <richard.sugg@sas.com>
2022-07-18 08:04:09 -0400

    add stackoverflow link

--- src/extension.ts
+++ src/extension.ts
@@ -81,6 +81,11 @@ export function buildCodeLens(
       command: "openPackage",
       arguments: [packageName, "homepage"],
     }),
+    new vscode.CodeLens(codeLensRange, {
+      title: "StackOverflow",
+      command: "openPackage",
+      arguments: [packageName, "stackoverflow"],
+    }),
   ];
 }
 
@@ -110,7 +115,9 @@ export function activate(context: vscode.ExtensionContext) {
 
   let regCommandDisposable = vscode.commands.registerCommand(
     "openPackage",
-    async (...args: [string, "npm" | "github" | "homepage"]) => {
+    async (
+      ...args: [string, "npm" | "github" | "homepage" | "stackoverflow"]
+    ) => {
       const [packageName, destination] = args;
       if (destination === "npm") {
         const link = vscode.Uri.parse(
@@ -127,6 +134,10 @@ export function activate(context: vscode.ExtensionContext) {
             .replace("git+", "")
             .replace(".git", "");
           const link = vscode.Uri.parse(repoURL);
+          await vscode.commands.executeCommand("vscode.open", link);
+        } else if (destination === "stackoverflow") {
+          const soURL = `https://stackoverflow.com/search?q=${packageName}`;
+          const link = vscode.Uri.parse(soURL);
           await vscode.commands.executeCommand("vscode.open", link);
         } else {
           const homeURL = (packageDetails.homepage as string).replace(
